ea92ddd9f6b2cc1781bde55dbceda81871268250
Robert van Eijk <251185@virtualwindow.co.za>
2026-03-11 23:37:48 +0200

    Finished Styling for Dashboard/Landing page
    Scrapped and redid styling for dashboard cards and swapped out ReactBootstrap cards for own cards..
    
    Changes occurred in dashcards.js and dashcards.css

--- pokebust/src/components/dashcards.js
+++ pokebust/src/components/dashcards.js
@@ -1,50 +1,49 @@
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+// import Button from "react-bootstrap/Button";
+// import Card from "react-bootstrap/Card";
 
 function DashCards() {
 	return (
-		<div class="dashcards">
-			<div class="dashCard">
-				<Card.Body>
-					<div class="cardContent">
-						<Card.Title>Card Title</Card.Title>
-						<Card.Text>
-							Some quick example text to build on the card title and make up the
-							bulk of the card's content.
-						</Card.Text>
-					</div>
-					<div class="cardImage" src="../images/Charmander.svg"></div>
-				</Card.Body>
+		<div class="dashCards">
+			<div class="card1">
+				<div class="cardInfo">
+					<h1 class="cardTitle">What is Pokebust</h1>
+					<p class="cardBodyCopy">
+						Discover PokeBust — your ultimate Pokémon stats hub! Explore base
+						stats, compare favourites, and watch how strength grows over levels
+						— all powered by PokeAPI.
+					</p>
+				</div>
 			</div>
-			<div class="dashCard">
-				<Card.Body>
-					<Card.Title>Card Title</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</Card.Text>
-					<Button variant="primary">Go somewhere</Button>
-				</Card.Body>
+			<div class="card2">
+				<div class="cardInfo">
+					<h1 class="cardTitle">Unsure of Your Pokemon Info??</h1>
+					<p class="cardBodyCopy">
+						Quickly search any Pokémon and get instant stats, types, Pokédex
+						info, and locations. Never guess again — just search and explore!
+					</p>
+					<button id="PikaButton">Search Now!</button>
+				</div>
 			</div>
-			<div class="dashCard">
-				<Card.Body>
-					<Card.Title>Card Title</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</Card.Text>
-					<Button variant="primary">Go somewhere</Button>
-				</Card.Body>
+			<div class="card3">
+				<div class="cardInfo">
+					<h1 class="cardTitle">Comparing The Data!!</h1>
+					<p class="cardBodyCopy">
+						Pick two Pokémon and see their stats go head-to-head! Radar charts,
+						side-by-side bars, and type match-ups make it easy to decide who’s
+						stronger.
+					</p>
+					<button id="SquirtleButton">Compare Now!</button>
+				</div>
 			</div>
-			<div class="dashCard">
-				<Card.Body>
-					<Card.Title>Card Title</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</Card.Text>
-					<Button variant="primary">Go somewhere</Button>
-				</Card.Body>
+			<div class="card4">
+				<div class="cardInfo">
+					<h1 class="cardTitle">View Your Poke-Timeline!!</h1>
+					<p class="cardBodyCopy">
+						Watch your Pokémon’s power grow level by level! Follow the timeline
+						of HP, Attack, Speed and more as it evolves and hits new peaks.
+					</p>
+					<button id="BulbaButton">View Timeline!</button>
+				</div>
 			</div>
 		</div>
 	);
